d798f520dad47b272daa53f30e39d32c0a337dae
voyciecho <voyciecho@yahoo.com>
2022-02-13 19:43:54 +0000

    Add french numbers single page app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,119 @@
 
 import React from 'react';
 
-class App extends React.Component {
+const frenchDictionary = {1: 'un', 2: 'deux', 3: 'trois', 4: 'quatre', 5: 'cinq', 6: 'six', 7: 'sept', 8: 'huit', 9: 'neuf', 10: 'dix'};
+
+class FrenchInputForm extends React.Component {
+    state = {
+        frenchText: 'french number'
+    }
+
+    handleChange = (event) => {
+        this.setState({frenchText: event.target.value});
+    }
+
+    handleSubmit = (event) => {
+        this.props.handleSubmit(this.state.frenchText);
+    }
+
     render() {
         return (
-            <div className='App'>
-                <p>hello world</p>
+            <div>
+            <form autoComplete="off">
+                <input
+                    type="text"
+                    name="name"
+                    value={this.state.frenchText}
+                    onChange={this.handleChange}
+                />
+                <input
+                    type="button"
+                    name="Submit"
+                    value="Submit"
+                    onClick={this.handleSubmit}
+                />
+            </form>
+            </div>
+        );
+    }
+}
+
+class UserStatistics extends React.Component {
+    render() {
+        return (
+            <table>
+            <tbody>
+                <tr>
+                    <th>Correct:</th>
+                    <th>{this.props.correctCount}</th>
+                </tr>
+                <tr>
+                    <th>Failed:</th>
+                    <th>{this.props.failedCount}</th>
+                </tr>
+            </tbody>
+            </table>
+        );
+    }
+}
+
+class HistoryTable extends React.Component {
+    render() {
+        let rows = [];
+
+        this.props.answersList.forEach((answer) => {
+            rows.push((
+                <tr>
+                    <th>{answer.number}</th>
+                    <th>{answer.french}</th>
+                </tr>
+                )
+            );
+        });
+        rows.reverse();
+
+        return (
+            <div style={{overflowY:'auto', height:'100px'}}>
+            <table>
+            <tbody>
+            {rows}
+            </tbody>
+            </table>
+            </div>
+        );
+    }
+}
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            numero: Math.floor((Math.random() * 10) + 1),
+            correctCount: 0,
+            failedCount: 0,
+            answersList: []
+        }
+    }
+
+    submitAnswer = (answer) => {
+        if (frenchDictionary[this.state.numero] === answer) {
+            this.setState({correctCount: this.state.correctCount + 1})
+        }
+        else {
+            this.setState({failedCount: this.state.failedCount + 1})
+        }
+        this.state.answersList.push({number: this.state.numero, french: answer});
+        this.setState({numero: Math.floor((Math.random() * 10) + 1) });
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>{this.state.numero}</h1>
+                <FrenchInputForm handleSubmit={this.submitAnswer} />
+                <UserStatistics correctCount={this.state.correctCount} failedCount={this.state.failedCount}/>
+                <HistoryTable answersList={this.state.answersList}/>
             </div>
         );
     }
